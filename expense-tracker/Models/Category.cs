fca2b5ae18e595da9f45ea79cddb06a93dc58f8c
KyryloMuzychka <kyryll2016@gmail.com>
2024-10-15 23:33:16 +0300

    set design - Syncfusion; added model Category, Transaction

---
+++ expense-tracker/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace expense_tracker.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Column(TypeName="VARCHAR(50)")]
+        public string Title { get; set; }
+
+        [Column(TypeName = "VARCHAR(5)")]
+        public string Icon { get; set; } = "";
+
+        [Column(TypeName = "VARCHAR(10)")]
+        public string Type { get; set; } = "Expense";
+    }
+}
