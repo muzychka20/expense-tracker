66390b9d68ce121ceebee0bbdb726ef1ba2b0a7f
KyryloMuzychka <kyryll2016@gmail.com>
2024-10-17 01:03:26 +0300

    combine edit and add new category functions

--- expense-tracker/Controllers/CategoryController.cs
+++ expense-tracker/Controllers/CategoryController.cs
@@ -42,40 +42,31 @@ namespace expense_tracker.Controllers
             return View(category);
         }
 
-        // GET: Category/Create
-        public IActionResult Create()
+        // GET: Category/AddOrEdit
+        public IActionResult AddOrEdit(int id = 0)
         {
-            return View(new Category());
+            if (id == 0)
+            { 
+                return View(new Category()); 
+            }
+            else
+            {
+                return View(_context.Categories.Find(id));
+            }
         }
 
-        // POST: Category/Create
+        // POST: Category/AddOrEdit
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("CategoryId,Title,Icon,Type")] Category category)
+        public async Task<IActionResult> AddOrEdit([Bind("CategoryId,Title,Icon,Type")] Category category)
         {
             if (ModelState.IsValid)
             {
                 _context.Add(category);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
-            }
-            return View(category);
-        }
-
-        // GET: Category/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories.FindAsync(id);
-            if (category == null)
-            {
-                return NotFound();
             }
             return View(category);
         }
