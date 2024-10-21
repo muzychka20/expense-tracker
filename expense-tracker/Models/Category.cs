62c1282e2dcecfd4cc2bd1348dea9c11b3bbcd63
KyryloMuzychka <kyryll2016@gmail.com>
2024-10-21 22:36:03 +0300

    added validation for category & transaction

--- expense-tracker/Models/Category.cs
+++ expense-tracker/Models/Category.cs
@@ -3,27 +3,28 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace expense_tracker.Models
 {
-    public class Category
-    {
-        [Key]
-        public int CategoryId { get; set; }
+	public class Category
+	{
+		[Key]
+		public int CategoryId { get; set; }
 
-        [Column(TypeName="NVARCHAR(50)")]
-        public string Title { get; set; }
+		[Column(TypeName = "NVARCHAR(50)")]
+		[Required(ErrorMessage = "Title is required!")]
+		public string Title { get; set; }
 
-        [Column(TypeName = "NVARCHAR(5)")]
-        public string Icon { get; set; } = "";
+		[Column(TypeName = "NVARCHAR(5)")]
+		public string Icon { get; set; } = "";
 
-        [Column(TypeName = "NVARCHAR(10)")]
-        public string Type { get; set; } = "Expense";
+		[Column(TypeName = "NVARCHAR(10)")]
+		public string Type { get; set; } = "Expense";
 
-        [NotMapped]
-        public string? TitleWithIcon
-        {
-            get
-            {
-                return this.Icon + " " + this.Title;
-            }
-        }
-    }
+		[NotMapped]
+		public string? TitleWithIcon
+		{
+			get
+			{
+				return this.Icon + " " + this.Title;
+			}
+		}
+	}
 }
--- expense-tracker/Models/Transaction.cs
+++ expense-tracker/Models/Transaction.cs
@@ -9,11 +9,12 @@ namespace expense_tracker.Models
         [Key]
         public int TransactionId { get; set; }
 
-        // CategoryId
+        [Range(1, int.MaxValue, ErrorMessage = "Please select a category!")]
         public int CategoryId { get; set; }
         public Category? Category { get; set; }
-
-        public int Amount { get; set; }
+		
+        [Range(1, int.MaxValue, ErrorMessage = "Amount should be greater than 0!")]
+		public int Amount { get; set; }
 
         [Column(TypeName = "NVARCHAR(75)")]
         public string? Note { get; set; }
